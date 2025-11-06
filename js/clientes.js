aee77119138bbc2bc585da553cd4d1657290d8e7
Gabriel <gabriel.henrique.pereira.2005@gmail.com>
2025-11-06 00:24:46 -0300

    melhorando o frontend da main, e tambem adicionando o clientes, porem ainda falta finalizar o frontend

---
+++ js/clientes.js
@@ -0,0 +1,39 @@
+const btnLogout       = document.getElementById('btn_logout');
+const btnInicio       = document.getElementById('btn-inicio'); 
+const btnClientes     = document.getElementById('btn-cliente'); 
+const btnNovoCliente  = document.getElementById('btn-novo-cliente');
+const popupBackground = document.getElementById('popupBackground');
+const popupContent    = document.getElementById('popupContent')
+
+btnLogout.addEventListener('click', ()=>{
+    window.location.href = '../backend/logout.php';
+})
+
+btnInicio.addEventListener('click', ()=>{
+    window.location.href = '../main/home.html';
+})
+
+btnClientes.addEventListener('click', ()=>{
+    window.location.reload();
+})
+
+btnNovoCliente.addEventListener('click', ()=>{
+    mostrarPopup()
+})
+
+document.getElementById('closeBtn').addEventListener('click', fecharPopup);
+document.addEventListener('keydown', fecharPopup);
+
+function mostrarPopup(){
+   document.body.style.overflow = 'hidden';
+   popupBackground.style.display = 'flex';
+
+}
+
+function fecharPopup(event){
+    if (event.type === 'keydown' && event.key !== 'Escape') return;
+
+    popupBackground.style.display = 'none';
+    popupContent.innerHTML        = '';
+    document.body.style.overflow  = 'auto';
+}
--- js/home.js
+++ js/home.js
@@ -1,5 +1,15 @@
-const btnLogout = document.getElementById('btn_logout');
+const btnLogout    = document.getElementById('btn_logout');
+const btnInicio    = document.getElementById('btn-inicio'); 
+const btnClientes  = document.getElementById('btn-cliente'); 
 
 btnLogout.addEventListener('click', ()=>{
     window.location.href = '../backend/logout.php';
+})
+
+btnInicio.addEventListener('click', ()=>{
+    window.location.reload();
+})
+
+btnClientes.addEventListener('click', ()=>{
+    window.location.href = '../main/clientes.html';
 })

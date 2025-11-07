8a0025b50a52cd6d7cf268242a375e78ebca0b75
Gabriel <gabriel.henrique.pereira.2005@gmail.com>
2025-11-07 14:10:11 -0300

    adicionando o where pelo id_empresa nas Selects do dashboard

--- backend/dashboard.php
+++ backend/dashboard.php
@@ -28,6 +28,8 @@
     $password = $_SESSION['banco_senha'];
     $dbname   = $_SESSION['banco_database'];  
 
+    $id_empresa = $_SESSION['USUARIOS_id_empresa'];
+
     $conn = pg_connect("host=$host port=$port user=$user password=$password dbname=$dbname");
 
     try{
@@ -56,8 +58,9 @@
                         agendamentos.valor
                     FROM agendamentos
                     INNER JOIN clientes ON clientes.id = agendamentos.id_cliente
-                    WHERE agendamentos.enviado = false";    
-        $result2 = pg_query_params($conn, $query2, array());
+                    WHERE agendamentos.enviado = false
+                    AND agendamentos.id_empresa = $1";    
+        $result2 = pg_query_params($conn, $query2, array($id_empresa));
 
         if(!$result2){
             http_response_code(500);
@@ -83,8 +86,9 @@
                     FROM agendamentos
                     INNER JOIN clientes ON clientes.id = agendamentos.id_cliente
                     WHERE agendamentos.enviado = true
-                    AND agendamentos.data = CURRENT_DATE";    
-        $result3 = pg_query_params($conn, $query3, array());
+                    AND agendamentos.data = CURRENT_DATE
+                    AND agendamentos.id_cliente = $1";    
+        $result3 = pg_query_params($conn, $query3, array($id_empresa));
 
         if(!$result3){
             http_response_code(500);
